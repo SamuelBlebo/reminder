fe0b326bf128e4ff06a3fffc76bdec6288115046
Samuel Blebo <samuel.blebo@yahoo.com>
2024-03-08 13:39:31 +0000

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <div>Hello World</div>
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ client/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaUser } from "react-icons/fa";
+
+export default function Header() {
+  return (
+    <>
+      <div className="h-[50px]  bg-[#d9d9d9] px-[90px] flex flex-row justify-between items-center">
+        <div className="h-[25px] w-[60px] bg-slate-50 rounded-md"></div>
+        <div className="h-[30px] w-[30px] bg-slate-50 flex items-center justify-center rounded-[50%]">
+          <FaUser className="text-[#d9d9d9]" />
+        </div>
+      </div>
+    </>
+  );
+}
